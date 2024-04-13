6794d96516f8b06dcb9f4eaac7e3ae5c9ee45c54
seilylook <seilylook@naver.com>
2024-04-13 16:57:17 +0900

    done: dbt & macros

---
+++ custom_postgres/macros/ratings_macro.sql
@@ -0,0 +1,8 @@
+{% macro generate_ratings(column_name) %}
+        CASE
+            WHEN user_rating >= 4.5 THEN 'Excellent'
+            WHEN user_rating >= 4.0 THEN 'Good'
+            WHEN user_rating >= 3.0 THEN 'Average'
+            ELSE 'Poor'
+        END AS rating_category
+{% endmacro %}
